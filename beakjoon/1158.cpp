aafcf75ad081393f732ad3d6025d8cfa40580c20
vvalvvizal <83233101+vvalvvizal@users.noreply.github.com>
2022-09-12 22:29:29 +0900

    solved problem

---
+++ beakjoon/1158.cpp
@@ -0,0 +1,33 @@
+#include <deque>
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    deque<int> q;
+
+    for (int i = 1; i <= n; i++) {
+        q.push_back(i);
+    }
+
+    cout << "<";
+    while (1) {
+        int tmp = 0;
+        for (int j = 0; j < k - 1; j++) { // k번 앞에서 뽑기
+            tmp = q.front();
+            q.pop_front();
+            q.push_back(tmp);
+        }
+        cout << q.front();
+        q.pop_front();
+
+        if (q.size() >= 1) {
+            cout << ", ";
+        }
+        if (q.empty())
+            break;
+    }
+
+    cout << ">";
+}
