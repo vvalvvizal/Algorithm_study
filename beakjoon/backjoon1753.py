364c5ed8f7fbe35482bcb05fd3a83923745546c0
vvalvvizal <vvalvvizal@gmail.com>
2024-02-05 14:52:46 +0900

    solved problem

---
+++ beakjoon/backjoon1753.py
@@ -0,0 +1,54 @@
+#시간초과
+
+# INF = float('inf')
+
+# def dijkstra(graph, start):
+#     n = len(graph)
+#     distance = [INF] * n
+#     visited = [False] * n
+
+#     distance[start] = 0
+
+#     for _ in range(n):
+#         min_dist = INF
+#         min_node = -1
+#         for j in range(n):
+#             if not visited[j] and distance[j] < min_dist:
+#                 min_dist = distance[j]
+#                 min_node = j
+
+#         if min_node == -1:
+#             break
+
+#         visited[min_node] = True
+
+#         for v, w in graph[min_node]:
+#             if not visited[v] and distance[min_node] + w < distance[v]:
+#                 distance[v] = distance[min_node] + w
+
+#     return distance
+
+# # 입력 받기
+# V, E = map(int, input().split())
+# K = int(input())
+
+# # 그래프 초기화
+# graph = [[] for _ in range(V + 1)]
+
+# # 그래프 정보 입력 받기
+# for _ in range(E):
+#     u, v, w = map(int, input().split())
+#     graph[u].append((v, w))
+
+# # 다익스트라 알고리즘 수행
+# shortest_paths = dijkstra(graph, K)
+
+# # 출력
+# for i in range(1, V + 1):
+#     if shortest_paths[i] == INF:
+#         print("INF")
+#     else:
+#         print(shortest_paths[i])
+
+###########################################################################
+#우선순위큐 (heapq)
--- beakjoon/backjoon9655.py
+++ beakjoon/backjoon9655.py
@@ -3,3 +3,30 @@ if n % 2 == 0:
     print('CY')
 else:
     print('SK')
+
+
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input())
+# dp = [-1] * 1001
+
+#SK
+# dp[1] = 1
+
+#CY
+# dp[2] = 0
+
+#SK
+# dp[3] = 1
+
+# for i in range(4, N + 1):
+#     if dp[i-1] or dp[i-3]:
+#         dp[i] = 0
+#     else:
+#         dp[i] = 1
+
+# if dp[N] == 0:
+#   print("CY")
+# else:
+#   print("SK")  
