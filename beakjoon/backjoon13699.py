2423a6c84f4b7758cd0d8c30f043222be6a6898b
vvalvvizal <vvalvvizal@gmail.com>
2024-02-21 20:46:05 +0900

    solved problem

--- beakjoon/backjoon13699.py
+++ beakjoon/backjoon13699.py
@@ -3,8 +3,8 @@
 def solved_problem(n, dp):
     dp[0] = 1 
 
-    for i in range(1, n+1):
-        for j in range(i):
+    for i in range(1, n+1):#1~n
+        for j in range(i): #0~i-1
             dp[i] += dp[j]*dp[i-j-1]
     return dp[n]
 
