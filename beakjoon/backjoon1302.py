ffd6146302e4abbc211accb1a6adc368535025f2
vvalvvizal <vvalvvizal@gmail.com>
2024-02-27 22:02:01 +0900

    fix

--- beakjoon/backjoon1302.py
+++ beakjoon/backjoon1302.py
@@ -9,7 +9,7 @@ for _ in range(n):
         lst[str]+=1
         
 sorted_key = sorted(lst.keys())
-#sorted(lst.items(), key= lambda x : x[1])
+
 max_key = max(sorted_key, key=lst.get)
 print(max_key)
 
