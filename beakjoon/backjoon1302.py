b9e9e867cdaf011216abc230f225af5700f7b391
vvalvvizal <vvalvvizal@gmail.com>
2024-02-27 22:23:49 +0900

    use get func

--- beakjoon/backjoon1302.py
+++ beakjoon/backjoon1302.py
@@ -11,6 +11,7 @@ for _ in range(n):
 sorted_key = sorted(lst.keys()) #키들만 정렬해서 반환 
 
 max_key = max(sorted_key, key= lambda x: lst[x]) #40ms 
-#max_key = max(sorted_key, key=lst.get) #44ms 
+#max_key = max(sorted_key, key=lst.get) #44ms
+#get()은 key에 대한 value를 반환해주는 함수
 print(max_key)
 
