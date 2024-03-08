a645e6eed1eaa73ce2c9bfd459e9c5e62cc26089
vvalvvizal <vvalvvizal@gmail.com>
2024-03-08 21:43:47 +0900

    solved problem

---
+++ beakjoon/backjoon30676.py
@@ -0,0 +1,15 @@
+n = int(input())
+if n>=620 and n<=780:
+    print("Red")
+elif n>=590 and n<620:
+    print("Orange")
+elif n>=570 and n<590:
+    print("Yellow")
+elif n>=495 and n<570:
+    print("Green")
+elif n>=450 and n<495:
+    print("Blue")
+elif n>=425 and n<450:
+    print("Indigo")
+elif n>=380 and n<425:
+    print("Violet")
