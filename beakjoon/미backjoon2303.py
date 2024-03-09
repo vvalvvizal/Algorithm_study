2814d5caa07a195e51fb766122ec82af41ceb89d
vvalvvizal <vvalvvizal@gmail.com>
2024-03-09 21:22:01 +0900

    solved problem

--- beakjoon/미backjoon2303.py
+++ beakjoon/미backjoon2303.py
@@ -1,25 +1,34 @@
+# n = int(input())
+# lst = []
+# for _ in range(n):
+#     lst.append(list(map(int, input().split())))
+
+# for idx, l in enumerate(lst):
+#     max_num = -1
+#     max_index = 0  # 최대값을 갖는 리스트의 인덱스를 저장할 변수
+#     answer_index =0
+#     answer_num = 0
+#     for i in  range(n):
+#         if max_num < current_sum:
+#             max_num = current_sum
+#             max_index = idx
+#     if max_num >= answer_index:
+#         answer_index = idx+1
+#         answer_num = max_num
+
+# print(answer_index)
+
+from itertools import combinations
 n = int(input())
-lst = []
-for _ in range(n):
-    lst.append(list(map(int, input().split())))
-
-result = []  # 최대값과 해당 인덱스를 저장할 리스트
-
-for idx, l in enumerate(lst):
-    max_num = -1
-    max_index = 0  # 최대값을 갖는 리스트의 인덱스를 저장할 변수
-    for i in range(len(l)):
-        for j in range(i + 1, len(l)):
-            for k in range(j + 1, len(l)):
-                # 현재 조합의 합 계산
-                current_sum = sum((l[i], l[j], l[k])) % 10
-                # 최대값 갱신
-                if max_num < current_sum:
-                    max_num = current_sum
-                    max_index = idx
-
-    result.append((max_num, max_index))
-
-# result 중복되는 최댓값들의 인덱스 중 가장 큰 값의 인덱스 반환
-max_index = max(result, key=lambda x: x[0])[1]
-print(max_index+1)
+lst = [list(map(int, input().split())) for _ in range(n)]
+ans = 0
+ans_max = 0
+for i in range(n):
+    comb = list(combinations(lst[i], 3))
+    temp = 0
+    for j in comb:
+        temp = max(temp, sum(j) % 10)#일의자리수 중 max값 찾기 
+    if temp >= ans_max:
+        ans = i + 1
+        ans_max = temp
+print(ans)
