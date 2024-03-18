b84dab10b05cc359cbd7f4dfe5b52307c2c0420b
vvalvvizal <vvalvvizal@gmail.com>
2024-03-18 16:34:31 +0900

    solved problem

--- beakjoon/미backjoon1380.py
+++ beakjoon/미backjoon1380.py
@@ -1,16 +1,24 @@
-t=0
-
+t=0 #시나리오 번호 
 while True:
-    t += 1 #시나리오 번호 
+
+    dict = set() #빼앗긴 리스트set
+    t += 1 #시나리오 번호 증가 
     lst = []
-    check = [0] * (n+1)
-    n = int(input())
+    result = [] #돌려받은 학생 리스트 
+    n = int(input()) #학생 수
     if n==0:
-        break 
+        break #종료조건 
     for _ in range(n):
         tmp = input()
-        lst.append(tmp)
-    
-    num, a = input().split()
-    check[int(num)]
+        lst.append(tmp) #학생 이름 리스트에 추가 
 
+    for _ in range(2*n-1):
+        num, alphabet = input().split()
+        if num not in dict:
+            dict.add(num)
+        else: #이미 거친 학생번호이면 
+            result.append(int(num))
+    # print(f"result : {result}")
+    for l in range(len(lst)):
+        if (l+1) not in result:
+            print(f"{t} {lst[l]}")
