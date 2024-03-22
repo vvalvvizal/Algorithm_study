873341938d0d9653a9594ff9198441f107e896f9
vvalvvizal <vvalvvizal@gmail.com>
2024-03-22 23:49:31 +0900

    not yet

--- beakjoon/미backjoon30804.py
+++ beakjoon/미backjoon30804.py
@@ -9,21 +9,18 @@ slow = 0
 maxi = 0
 q = deque(lst)
 while q:
-    for i in range(n+1):#브루트포스  0~n
-        q = deque(lst)
-        if q:
-            tmp = q.pop()#앞에서 i개 
-        for j in range(n+1):
-            if q:
-                tmp = q.popleft()#뒤에서 j개
-                print(f"{i}{j} : {q}")
-
     for fast in range(1, len(q)):
         if q[slow] != q[fast]:
             slow +=1
             q[slow] = q[fast] #지금 도착한 fast를 slow에 저장 
-
+    print(q)
     if slow+1>maxi:
         maxi = slow+1
+    for i in range(n+1):#브루트포스  0~n
+        q = deque(lst)
+        tmp = q.pop()#앞에서 i개 
+        for j in range(n+1):
+            if q:
+                tmp = q.popleft()#뒤에서 j개
 
     print(maxi)
