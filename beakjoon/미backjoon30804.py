d0687c5ff315e8d3b061a8d180e170c8f3cbb73b
vvalvvizal <vvalvvizal@gmail.com>
2024-05-06 22:33:41 +0900

    .

--- beakjoon/미backjoon30804.py
+++ beakjoon/미backjoon30804.py
@@ -12,11 +12,11 @@ while q:
     for fast in range(1, len(q)):
         if q[slow] != q[fast]:
             slow +=1
-            q[slow] = q[fast] #지금 도착한 fast를 slow에 저장 
+            q[slow] = q[fast]
     print(q)
     if slow+1>maxi:
         maxi = slow+1
-    for i in range(n+1):#브루트포스  0~n
+    for i in range(n+1):#브루트포스  
         q = deque(lst)
         tmp = q.pop()
         for j in range(n+1):
