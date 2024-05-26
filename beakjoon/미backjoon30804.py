ae6fea734d498718692001f8fb885e07f2a37bad
vvalvvizal <vvalvvizal@gmail.com>
2024-05-26 22:08:35 +0900

    .

--- beakjoon/미backjoon30804.py
+++ beakjoon/미backjoon30804.py
@@ -3,24 +3,7 @@ from collections import deque
 n = int(input())
 lst = list(map(int,input().split()))
 
-type = 0 
-
-slow = 0
-maxi = 0
+# 투포인터 l,r 과 빠른, 느린 
+s = 0
+f = 0
 q = deque(lst)
-while q:
-    for fast in range(1, len(q)):
-        if q[slow] != q[fast]:
-            slow +=1
-            q[slow] = q[fast]
-    print(q)
-    if slow+1>maxi:
-        maxi = slow+1
-    for i in range(n+1):#브루트포스  
-        q = deque(lst)
-        tmp = q.pop()
-        for j in range(n+1):
-            if q:
-                tmp = q.popleft()
-
-    print(maxi)
