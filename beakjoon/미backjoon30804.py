2be022d3ee5f2ffe664d98a7bd65301126e6b5f8
vvalvvizal <vvalvvizal@gmail.com>
2024-05-28 00:30:24 +0900

    solved problem

--- beakjoon/미backjoon30804.py
+++ beakjoon/미backjoon30804.py
@@ -1,9 +1,26 @@
-from collections import deque
+def max_fruits_with_two_types(n, fruits):
+    max_length = 0
+    left = 0
+    fruit_count = {}
+    
+    for right in range(n):
+        if fruits[right] in fruit_count:
+            fruit_count[fruits[right]] += 1
+        else:
+            fruit_count[fruits[right]] = 1
+        
+        while len(fruit_count) > 2:
+            fruit_count[fruits[left]] -= 1
+            if fruit_count[fruits[left]] == 0:
+                del fruit_count[fruits[left]]
+            left += 1
+        
+        max_length = max(max_length, right - left + 1)
+    
+    return max_length
+
 
 n = int(input())
-lst = list(map(int,input().split()))
+fruits = list(map(int, input().split()))
 
-# 투포인터 l,r 과 빠른, 느린 
-s = 0
-f = 0
-q = deque(lst)
+print(max_fruits_with_two_types(n, fruits))
