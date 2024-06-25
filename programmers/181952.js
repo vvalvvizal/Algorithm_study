352ee77af5f22178ca11fee03f95a060f21dbf28
vvalvvizal <vvalvvizal@gmail.com>
2024-06-25 18:59:03 +0900

    solved problem

--- programmers/181952.js
+++ programmers/181952.js
@@ -1,5 +1,6 @@
 const readline = require("readline");
 const rl = readline.createInterface({
+  //인터페이스 생성
   input: process.stdin,
   output: process.stdout,
 });
@@ -9,6 +10,7 @@ let str = "";
 
 rl.on("line", function (line) {
   input = [line];
+  rl.close(); //한번 입력 받고 인터페이스 닫음.
 }).on("close", function () {
   str = input[0];
   console.log(str);
